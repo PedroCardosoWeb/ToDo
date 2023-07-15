e3c19fd64e6ca297e63ee92a08536ad6994a58d6
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2023-07-15 12:17:50 -0300

    :sparkles: feat: creating a task

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
 const express =  require('express') //importa o módulo express no arquivo
 const server = express() //inicializa o express em server
+server.use(express.json())
 
-server.get('/teste', (req, res) => {
-    res.send('TUDO CERTO COM A NOSSA API!')
-})
+const TaskRoutes = require('./routes/TaskRoutes')
+server.use('/task', TaskRoutes)
 
 server.listen(3000, () => {
     console.log('API ONLINE')
