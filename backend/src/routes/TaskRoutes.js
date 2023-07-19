f76f40cde401af19cdf957c63e92f5a4afc22a1c
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2023-07-19 10:04:28 -0300

    :sparkles: feat: updating status from a task

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -6,9 +6,10 @@ const TaskValidation = require('../middlewares/TaskValidation')
 const MacAddressValidation = require('../middlewares/MacAddressValidation')
 
 router.post('/', TaskValidation, TaskController.create)
-router.put('/:id', TaskController.update) // ':id' (dois pontos) indica uma variável, de nome 'id'
 router.get('/:id', TaskController.show)
 router.get('/filter/all', MacAddressValidation, TaskController.all)
+router.put('/:id', TaskController.update) // ':id' (dois pontos) indica uma variável, de nome 'id'
+router.put('/:id/:done', TaskController.done) 
 router.delete('/:id', TaskController.delete) 
 
 module.exports = router
