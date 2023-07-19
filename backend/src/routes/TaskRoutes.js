4de7ce7175b165c22434634a50834edc1e34e944
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2023-07-19 09:48:38 -0300

    :sparkles: feat: show task by id

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -7,6 +7,7 @@ const MacAddressValidation = require('../middlewares/MacAddressValidation')
 
 router.post('/', TaskValidation, TaskController.create)
 router.put('/:id', TaskController.update) // ':id' (dois pontos) indica uma variável, de nome 'id'
+router.get('/:id', TaskController.show)
 router.get('/filter/all', MacAddressValidation, TaskController.all)
 
 module.exports = router
