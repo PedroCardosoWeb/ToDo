977f1e8b5d1c190c6713386b85920cc641ec7265
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2023-08-02 13:17:15 -0300

    :recycle: refactor: creating environment variables - database access

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://localhost:27017/todo' //url de conexão com o bd
+// const url = 'mongodb://localhost:27017/todo' //url de conexão com o bd
+const url = process.env.MONGO_URL
+mongoose.set('useUnifiedTopology', true)
 mongoose.connect(url, { useNewUrlParser: true}) //obj de config para compatibilidade com outras versões do mongodb
 
 module.exports = mongoose
