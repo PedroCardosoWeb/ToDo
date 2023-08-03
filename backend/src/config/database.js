860f879b566332ef1acca9d9453b8cf23d7c58e5
Pedro Cardoso <pedrocastilhoweb@gmail.com>
2023-08-03 15:03:48 -0300

    :recycle: refactor: assure connect atlas_cloud_mongodb before continue

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 
-// const url = 'mongodb://localhost:27017/todo' //url de conexão com o bd
 const url = process.env.MONGO_URL
-mongoose.set('useUnifiedTopology', true)
-mongoose.connect(url, { useNewUrlParser: true}) //obj de config para compatibilidade com outras versões do mongodb
+    mongoose.connect(url, 
+        { useNewUrlParser: true, useUnifiedTopology: true }) //obj de config para compatibilidade com outras versões do mongodb
+    .then(() => 
+        console.log("Conectado ao mongoDB com sucesso!"))
+    .catch((error) => 
+        console.log("Erro ao conectar com o mongoDB!", error))
 
 module.exports = mongoose
